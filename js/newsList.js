196a5f558c20862da9d637f1d023f5070938d8bd
Unknown <hn85412404@gmail.com>
2020-01-13 21:50:02 +0800

    newsList手機切換slider

--- js/newsList.js
+++ js/newsList.js
@@ -1,47 +1,78 @@
 //news 內文字數
-var pTxt = []
-var pTxtLimit = 75
-for (var i = 0; i < $('.newsTxt p').length; i++) {
-  pTxt[i] = $('.newsTxt').eq(i).find('p').text()
+var pTxt = [];
+var pTxtLimit = 75;
+for (var i = 0; i < $(".newsTxt p").length; i++) {
+  pTxt[i] = $(".newsTxt")
+    .eq(i)
+    .find("p")
+    .text();
   // console.log(pTxt)
 }
 
 function kvTxt() {
-  for (var i = 0; i < $('.newsTxt p').length; i++) {
-    var pTxtN = pTxt[i].length
+  for (var i = 0; i < $(".newsTxt p").length; i++) {
+    var pTxtN = pTxt[i].length;
     // console.log(pTxtN)
     if (pTxtN > pTxtLimit && $(window).width() <= 768) {
-      pTxtC = pTxt[i].substr(0, 40)
+      pTxtC = pTxt[i].substr(0, 40);
       // console.log(pTxtC)
-      $('.newsTxt').eq(i).find('p').text(pTxtC + '...')
+      $(".newsTxt")
+        .eq(i)
+        .find("p")
+        .text(pTxtC + "...");
     } else {
-      pTxtC = pTxt[i].substr(0, pTxtLimit)
-      $('.newsTxt').eq(i).find('p').text(pTxtC + '...')
+      pTxtC = pTxt[i].substr(0, pTxtLimit);
+      $(".newsTxt")
+        .eq(i)
+        .find("p")
+        .text(pTxtC + "...");
       // $('.newsTxt').eq(i).find('p').text(pTxt[i])
     }
   }
 }
-$(window).resize(function () {
-  kvTxt()
-}).resize()
 
+let isMb = false;
+
+$(window).ready(function() {
+  initSlider();
+});
+
+$(window)
+  .resize(function() {
+    kvTxt();
+    const ww = window.innerWidth;
+    if (ww <= 720 && isMb == false) {
+      initSlider();
+      isMb = true;
+    } else {
+      $("#mbNav").slick("unslick");
+      isMb = false;
+    }
+  })
+  .resize();
+
+function initSlider() {
+  $("#mbNav").slick({
+    dots: false,
+    arrows: true,
+    speed: 500,
+    prevArrow:
+      '<div class="sec-prev secBtn"><img src="images/arrow_2.png" alt=""></div>',
+    nextArrow:
+      '<div class="sec-next secBtn"><img src="images/arrow.png" alt=""></div>'
+  });
+}
 
 //
-$('.googleAD_kv > div:not(.gAd_KVCloseBtn)').mCustomScrollbar({
-  axis: 'x',
-  theme: 'minimal-dark',
+$(".googleAD_kv > div:not(.gAd_KVCloseBtn)").mCustomScrollbar({
+  axis: "x",
+  theme: "minimal-dark"
 });
-$('.newsListBt > div').mCustomScrollbar({
-  axis: 'x',
-  theme: 'minimal-dark',
+$(".newsListBt > div").mCustomScrollbar({
+  axis: "x",
+  theme: "minimal-dark"
 });
-$('.sec_02 > div').mCustomScrollbar({
-  axis: 'x',
-  theme: 'minimal-dark',
+$(".sec_02 > div").mCustomScrollbar({
+  axis: "x",
+  theme: "minimal-dark"
 });
-
-
-
-
-
-
