fa63c21a3a069a310d6e97884eaa5b1275cefcdd
Unknown <hn85412404@gmail.com>
2020-03-19 23:40:17 +0800

    wip

--- js/news_inner.js
+++ js/news_inner.js
@@ -1,27 +1,28 @@
 //
-$('.googleAD_kv > div:not(.gAd_KVCloseBtn)').mCustomScrollbar({
-  axis: 'x',
-  theme: 'minimal-dark',
+$(".googleAD_kv > div:not(.gAd_KVCloseBtn)").mCustomScrollbar({
+  axis: "x",
+  theme: "minimal-dark"
 });
-$('.newsListBt > div').mCustomScrollbar({
-  axis: 'x',
-  theme: 'minimal-dark',
+$(".newsListBt > div").mCustomScrollbar({
+  axis: "x",
+  theme: "minimal-dark"
 });
-$('.sec_02 > div').mCustomScrollbar({
-  axis: 'x',
-  theme: 'minimal-dark',
+$(".sec_02 > div").mCustomScrollbar({
+  axis: "x",
+  theme: "minimal-dark"
 });
-
 
 //相關文章區
-$('.slickRelatd').slick({
+$(".slickRelatd").slick({
   slidesToShow: 4,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 5000,
   speed: 500,
-  prevArrow: '<div class="rela-prev relaBtn"><span class="iconFont icon-cheveron-left"></div>',
-  nextArrow: '<div class="rela-next relaBtn"><span class="iconFont icon-cheveron-right"></div>',
+  prevArrow:
+    '<div class="rela-prev relaBtn"><span class="iconFont icon-cheveron-left"></div>',
+  nextArrow:
+    '<div class="rela-next relaBtn"><span class="iconFont icon-cheveron-right"></div>',
   responsive: [
     {
       breakpoint: 768,
@@ -33,53 +34,51 @@ $('.slickRelatd').slick({
   ]
 });
 
-$(function(){
-  var smbTxt = $('.smbTxt')
-  var txtGrade
+$(function() {
+  var smbTxt = $(".smbTxt");
+  var txtGrade;
   // console.log(txtGrade)
-  smbTxt.on('click', function(){
-    smbTxt.removeClass('active')
-    $(this).addClass('active')
-    if(txtGrade!=undefined){
-      $('.articleBox p, .articleBox figure').removeClass(txtGrade)
+  smbTxt.on("click", function() {
+    smbTxt.removeClass("active");
+    $(this).addClass("active");
+    if (txtGrade != undefined) {
+      $(".articleBox").removeClass(txtGrade);
     }
-    if($(this).hasClass('smbTxtB')){
-      txtGrade = 'smbTxtB'
-    }else if($(this).hasClass('smbTxtM')){
-      txtGrade = 'smbTxtM'
-    }else if($(this).hasClass('smbTxtS')){
-      txtGrade = 'smbTxtS'
+    if ($(this).hasClass("smbTxtB")) {
+      txtGrade = "smbTxtB";
+    } else if ($(this).hasClass("smbTxtM")) {
+      txtGrade = "smbTxtM";
+    } else if ($(this).hasClass("smbTxtS")) {
+      txtGrade = "smbTxtS";
     }
-    $('.articleBox p, .articleBox figure').addClass(txtGrade)
+    $(".articleBox").addClass(txtGrade);
   });
 
-  $(window).on('load', function(){
-    detectPinInit()
-  })
+  $(window).on("load", function() {
+    detectPinInit();
+  });
 
   var pinInit = false;
 
-  $(window).on('resize', function(){
-    detectPinInit()
+  $(window).on("resize", function() {
+    detectPinInit();
   });
 
   function detectPinInit() {
     var ww = window.innerWidth > 540;
-    var article_h = $('#article_content').height();
-    var right_h = $('#right_content').height();
+    var article_h = $("#article_content").height();
+    var right_h = $("#right_content").height();
     console.log(ww, article_h, right_h);
-    if( right_h - article_h >= 100 && ww && !pinInit) {
+    if (right_h - article_h >= 100 && ww && !pinInit) {
       pinInit = true;
-      new $.Zebra_Pin($('.l_b'), {
+      new $.Zebra_Pin($(".l_b"), {
         contain: true,
         top_spacing: 100,
         z_index: 10
       });
     }
-    if(pinInit && !ww ){
+    if (pinInit && !ww) {
       location.reload();
     }
-  } 
-  
-})
-
+  }
+});
